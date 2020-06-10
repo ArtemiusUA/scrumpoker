6168440925d1327daa7cc4f0958950bb43312921
Artem Merkulov <artem.merkulov@gmail.com>
2020-06-10 11:14:04 +0300

    base vue logic

--- scrumpoker/static/js/room.js
+++ scrumpoker/static/js/room.js
@@ -2,6 +2,40 @@
     const url = (new URL(window.location));
     let socket = new WebSocket("ws://" + url.hostname + ":" + url.port + "/ws/" + roomId);
 
+    const roomApp = new Vue({
+        el: "#room",
+        data: {
+            state: {
+                id: "",
+                participants: {},
+                is_exposed: false,
+                moderator: "",
+            }
+        },
+        methods: {
+            renderResult: function (result) {
+                if (result === true){
+                    return "X";
+                } else if (result === false) {
+                    return "-";
+                } else {
+                    return result;
+                }
+            },
+            isExposed: result => result === "true",
+            vote: function (points) {
+                socket.send(JSON.stringify({
+                    type: "vote",
+                    data: {
+                        points,
+                    }
+                }))
+            }
+        }
+
+    });
+
+
     socket.onopen = function () {
         console.log("WS connected");
     }
@@ -16,7 +50,11 @@
     };
 
     socket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
         console.log("WS message: " + event.data);
+        if (data.type === "room_update") {
+            roomApp.state = data.data;
+        }
     };
 
     socket.onerror = function (error) {
